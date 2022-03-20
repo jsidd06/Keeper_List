ce61a79fbea49e251e9d4d2166f710b3658e550a
siddharth <jsidd06@gmail.com>
2022-03-20 20:18:38 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Components/Home/Home'
 
 function App() {
   return (
-    <div>Hello</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
